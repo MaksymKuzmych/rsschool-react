bbef2fdb74c95df77d3e88632a556849ecf2746c
Maksym Kuzmych <kuzmichmaksym1997@gmail.com>
2023-03-18 17:35:12 +0200

    feat: add tests to the project

---
+++ src/tests/SearchBar.test.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { describe, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+
+import { SearchBar } from '../components/SearchBar/SearchBar';
+
+describe('Search Bar', () => {
+  it('Should be defined', () => {
+    expect(<SearchBar />).toBeDefined();
+  });
+
+  it('Should has right placeholder', () => {
+    render(<SearchBar />);
+    expect(screen.getByPlaceholderText('What are you looking for?')).toBeInTheDocument();
+  });
+});
